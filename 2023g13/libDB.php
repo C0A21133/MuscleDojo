ad387128e8b03f82bf6db3791588871799e5054d
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2023-06-09 20:32:29 +0900

    テンプレート

---
+++ 2023g13/libDB.php
@@ -0,0 +1,18 @@
+<?php
+class libDB{
+    private PDO $pdo;
+    /**
+     * コンストラクタ
+     */
+    public function __construct()
+    {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=2022gxx;charset=utf8","root","", 
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
+
+    }
+
+    public function getPDO(){
+        return $this->pdo;
+    }
+}
+?>
