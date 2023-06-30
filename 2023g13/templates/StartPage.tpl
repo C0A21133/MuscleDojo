cd663371b551504698b4ccf51bde680a5b02abe3
umiha <c0a211433d@edu.teu.ac.jp>
2023-06-30 17:18:58 +0900

    logo

--- 2023g13/templates/StartPage.tpl
+++ 2023g13/templates/StartPage.tpl
@@ -1,110 +1,53 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <link rel="stylesheet" href="../cssFolder/StartPage.css">
     <meta charset="utf-8">
-    <style>
-    .canvas-wrap-front{
-            width: 600px;
-            max-width: 100%;
-            position: relative;
-            padding: 0;
-            box-sizing: content-box;
-            top:100px;
-            left:100px;
-            z-index: 3;
-
-    }
-    .canvas-wrap-back{
-            width: 600px;
-            max-width: 100%;
-            position: relative;
-            padding: 0;
-            box-sizing: content-box;
-            top: 400px;
-            left:100px;
-            z-index: 3;
-
-    }
-    
-    .canvas{
-        position: absolute;
-            left:0;
-        top:0;
-        border: 0;
-        max-width:100%;
-        box-sizing: content-box;
-        padding: 0;
-        margin: 0;
-        z-index: 3;
-    }
-
-    .BGImage{
-        position: relative;
-        z-index: 1;
-        background-image: url("../Resources/Images/Background/bg.jpg");
-        background-size: cover;
-        background-repeat:no-repeat;
-        background-position: center;
-        width: 200vh;
-        height: 200vh;
-    }
-    .BGImage::after{
-        content: "";
-        position: absolute;
-        z-index: 2;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        background-color: white;
-        opacity: 0.5;
-    }
-    
-    .front{
-        position: relative;
-        z-index: 3;
-    }
-    </style>
-    
     </head>
+    <div class="header-box"></div>
+    <header>
+        <h1 class = "logo">まっする</h1>
+        <nav class="pc-menu">
+            <ul>
+                
+            </ul>
+        </nav>
+    </header>
+    
     <body onload='Draw()'>
     <div class="BGImage">
-            <h2 class="front">aaaaaaaaaaa</h2>
-    
-    <div class="canvas-wrap-front" id="canvasdiv">
-        <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
+        <h1 class="front" style="text-align: center;"><font size="7" color = "#ffa500" >マッスル道場</font></h1>
+        <div class="canvas-wrap-front" id="canvasdiv">
+            <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
 
-        <!-- 画像を増やす場合、idの先頭の数字を一つづつ増やした<canvas>をコピペで記述
-            startPage.js の frontMuscleArray配列に パスを追記。
-            clickOnCanvas()とmoveOnCanvas()に行いたい処理を記述すること
-            idはStartPage.jsのfrontMuscleArrayの要素のインデックス番号+frontCanvas
-        -->
-        <canvas  id="0frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="1frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <!-- 判定用のキャンバス -->
-        <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
-    </div>
-    <div class="canvas-wrap-back">
-        <canvas  id="backCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- 画像を増やす場合、idの先頭の数字を一つづつ増やした<canvas>をコピペで記述
+                startPage.js の frontMuscleArray配列に パスを追記。
+                clickOnCanvas()とmoveOnCanvas()に行いたい処理を記述すること
+                idはStartPage.jsのfrontMuscleArrayの要素のインデックス番号+frontCanvas
+            -->
+            <canvas  id="0frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="1frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- 判定用のキャンバス -->
+            <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
+        </div>
+        <div class="canvas-wrap-back">
+            <canvas  id="backCanvas" width="282" height="400" class="canvas"></canvas>
 
-        <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
 
-        <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
-    </div>
-
-    
+            <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
+        </div>
     </div>
     
-
     <script src="../Script/StartPage.js"></script>
     </body>
     
--- 2023g13/templates_c/ad81a1f0b97936e2cf6da1225b1af5295493819d_0.file.StartPage.tpl.php
+++ 2023g13/templates_c/ad81a1f0b97936e2cf6da1225b1af5295493819d_0.file.StartPage.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2023-06-30 09:20:12
+/* Smarty version 3.1.39, created on 2023-06-30 10:17:38
   from 'C:\xampp\htdocs\pnw\MuscleDojo\2023g13\templates\StartPage.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_649e822c4546d1_35326810',
+  'unifunc' => 'content_649e8fa2763405_97584216',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ad81a1f0b97936e2cf6da1225b1af5295493819d' => 
     array (
       0 => 'C:\\xampp\\htdocs\\pnw\\MuscleDojo\\2023g13\\templates\\StartPage.tpl',
-      1 => 1688109608,
+      1 => 1688113056,
       2 => 'file',
     ),
   ),
@@ -20,114 +20,57 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_649e822c4546d1_35326810 (Smarty_Internal_Template $_smarty_tpl) {
+function content_649e8fa2763405_97584216 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
     <head>
+    <link rel="stylesheet" href="../cssFolder/StartPage.css">
     <meta charset="utf-8">
-    <style>
-    .canvas-wrap-front{
-            width: 600px;
-            max-width: 100%;
-            position: relative;
-            padding: 0;
-            box-sizing: content-box;
-            top:100px;
-            left:100px;
-            z-index: 3;
-
-    }
-    .canvas-wrap-back{
-            width: 600px;
-            max-width: 100%;
-            position: relative;
-            padding: 0;
-            box-sizing: content-box;
-            top: 400px;
-            left:100px;
-            z-index: 3;
-
-    }
-    
-    .canvas{
-        position: absolute;
-            left:0;
-        top:0;
-        border: 0;
-        max-width:100%;
-        box-sizing: content-box;
-        padding: 0;
-        margin: 0;
-        z-index: 3;
-    }
-
-    .BGImage{
-        position: relative;
-        z-index: 1;
-        background-image: url("../Resources/Images/Background/bg.jpg");
-        background-size: cover;
-        background-repeat:no-repeat;
-        background-position: center;
-        width: 200vh;
-        height: 200vh;
-    }
-    .BGImage::after{
-        content: "";
-        position: absolute;
-        z-index: 2;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        background-color: white;
-        opacity: 0.5;
-    }
-    
-    .front{
-        position: relative;
-        z-index: 3;
-    }
-    </style>
-    
     </head>
+    <div class="header-box"></div>
+    <header>
+        <h1 class = "logo">まっする</h1>
+        <nav class="pc-menu">
+            <ul>
+                
+            </ul>
+        </nav>
+    </header>
+    
     <body onload='Draw()'>
     <div class="BGImage">
-            <h2 class="front">aaaaaaaaaaa</h2>
-    
-    <div class="canvas-wrap-front" id="canvasdiv">
-        <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
+        <h1 class="front" style="text-align: center;"><font size="7" color = "#ffa500" >マッスル道場</font></h1>
+        <div class="canvas-wrap-front" id="canvasdiv">
+            <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
 
-        <!-- 画像を増やす場合、idの先頭の数字を一つづつ増やした<canvas>をコピペで記述
-            startPage.js の frontMuscleArray配列に パスを追記。
-            clickOnCanvas()とmoveOnCanvas()に行いたい処理を記述すること
-            idはStartPage.jsのfrontMuscleArrayの要素のインデックス番号+frontCanvas
-        -->
-        <canvas  id="0frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="1frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
-        <!-- 判定用のキャンバス -->
-        <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
-    </div>
-    <div class="canvas-wrap-back">
-        <canvas  id="backCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- 画像を増やす場合、idの先頭の数字を一つづつ増やした<canvas>をコピペで記述
+                startPage.js の frontMuscleArray配列に パスを追記。
+                clickOnCanvas()とmoveOnCanvas()に行いたい処理を記述すること
+                idはStartPage.jsのfrontMuscleArrayの要素のインデックス番号+frontCanvas
+            -->
+            <canvas  id="0frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="1frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
+            <!-- 判定用のキャンバス -->
+            <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
+        </div>
+        <div class="canvas-wrap-back">
+            <canvas  id="backCanvas" width="282" height="400" class="canvas"></canvas>
 
-        <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
+            <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
 
-        <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
-    </div>
-
-    
+            <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
+        </div>
     </div>
     
-
     <?php echo '<script'; ?>
  src="../Script/StartPage.js"><?php echo '</script'; ?>
 >
