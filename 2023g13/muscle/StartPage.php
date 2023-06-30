a54c9103028b3d0e96be0db0e94838721f5d6b5d
taroho <taroho.39@gmail.com>
2023-06-30 14:55:11 +0900

    ｋ

--- 2023g13/muscle/StartPage.php
+++ 2023g13/muscle/StartPage.php
@@ -4,9 +4,6 @@ require_once("../pnwsmarty.php");
 $pnw = new pnwsmarty();
 $smarty = $pnw->getTpl();
 
-$images = glob('../Resources/Images/FrontMusclePart/*.png');
-$images_json = json_encode($images);
-
 
 $smarty->display('StartPage.tpl');
 ?>
