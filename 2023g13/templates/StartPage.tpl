01f4f7bda2555a8bbe934b725ccec7b96d5793ad
umiha <c0a211433d@edu.teu.ac.jp>
2023-06-30 16:21:45 +0900

    haikei

--- 2023g13/templates/StartPage.tpl
+++ 2023g13/templates/StartPage.tpl
@@ -9,8 +9,10 @@
             position: relative;
             padding: 0;
             box-sizing: content-box;
-            top:0px;
-            left:0px;
+            top:100px;
+            left:100px;
+            z-index: 3;
+
     }
     .canvas-wrap-back{
             width: 600px;
@@ -19,7 +21,9 @@
             padding: 0;
             box-sizing: content-box;
             top: 400px;
-            left:0px;
+            left:100px;
+            z-index: 3;
+
     }
     
     .canvas{
@@ -31,13 +35,43 @@
         box-sizing: content-box;
         padding: 0;
         margin: 0;
+        z-index: 3;
+    }
+
+    .BGImage{
+        position: relative;
+        z-index: 1;
+        background-image: url("../Resources/Images/Background/bg.jpg");
+        background-size: cover;
+        background-repeat:no-repeat;
+        background-position: center;
+        width: 200vh;
+        height: 200vh;
+    }
+    .BGImage::after{
+        content: "";
+        position: absolute;
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: white;
+        opacity: 0.5;
     }
     
+    .front{
+        position: relative;
+        z-index: 3;
+    }
     </style>
+    
     </head>
     <body onload='Draw()'>
-
-    <div class="canvas-wrap" id="canvasdiv">
+    <div class="BGImage">
+            <h2 class="front">aaaaaaaaaaa</h2>
+    
+    <div class="canvas-wrap-front" id="canvasdiv">
         <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
         <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
 
@@ -51,6 +85,9 @@
         <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
         <!-- 判定用のキャンバス -->
         <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
     </div>
@@ -59,10 +96,14 @@
 
         <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="2backCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
 
         <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
     </div>
+
+    
+    </div>
+    
 
     <script src="../Script/StartPage.js"></script>
     </body>
--- 2023g13/templates_c/ad81a1f0b97936e2cf6da1225b1af5295493819d_0.file.StartPage.tpl.php
+++ 2023g13/templates_c/ad81a1f0b97936e2cf6da1225b1af5295493819d_0.file.StartPage.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2023-06-30 07:19:00
+/* Smarty version 3.1.39, created on 2023-06-30 09:20:12
   from 'C:\xampp\htdocs\pnw\MuscleDojo\2023g13\templates\StartPage.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_649e65c4c7e866_15894289',
+  'unifunc' => 'content_649e822c4546d1_35326810',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ad81a1f0b97936e2cf6da1225b1af5295493819d' => 
     array (
       0 => 'C:\\xampp\\htdocs\\pnw\\MuscleDojo\\2023g13\\templates\\StartPage.tpl',
-      1 => 1688102294,
+      1 => 1688109608,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl) {
+function content_649e822c4546d1_35326810 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
     <head>
@@ -32,8 +32,10 @@ function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl)
             position: relative;
             padding: 0;
             box-sizing: content-box;
-            top:0px;
-            left:0px;
+            top:100px;
+            left:100px;
+            z-index: 3;
+
     }
     .canvas-wrap-back{
             width: 600px;
@@ -42,7 +44,9 @@ function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl)
             padding: 0;
             box-sizing: content-box;
             top: 400px;
-            left:0px;
+            left:100px;
+            z-index: 3;
+
     }
     
     .canvas{
@@ -54,13 +58,43 @@ function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl)
         box-sizing: content-box;
         padding: 0;
         margin: 0;
+        z-index: 3;
+    }
+
+    .BGImage{
+        position: relative;
+        z-index: 1;
+        background-image: url("../Resources/Images/Background/bg.jpg");
+        background-size: cover;
+        background-repeat:no-repeat;
+        background-position: center;
+        width: 200vh;
+        height: 200vh;
+    }
+    .BGImage::after{
+        content: "";
+        position: absolute;
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: white;
+        opacity: 0.5;
     }
     
+    .front{
+        position: relative;
+        z-index: 3;
+    }
     </style>
+    
     </head>
     <body onload='Draw()'>
-
-    <div class="canvas-wrap" id="canvasdiv">
+    <div class="BGImage">
+            <h2 class="front">aaaaaaaaaaa</h2>
+    
+    <div class="canvas-wrap-front" id="canvasdiv">
         <canvas  id="frontCanvas" width="282" height="400" class="canvas"></canvas>
         <!-- スクリプトで描写するほうが記述量が少ないが、描写処理はまとめて記述したいのでHTMLで記述した -->
 
@@ -74,6 +108,9 @@ function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl)
         <canvas  id="2frontCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="3frontCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="4frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="5frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="6frontCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas  id="7frontCanvas" width="282" height="400" class="canvas"></canvas>
         <!-- 判定用のキャンバス -->
         <canvas  id="frontJudgeCanvas" width="282" height="400" class="canvas"></canvas>
     </div>
@@ -82,10 +119,14 @@ function content_649e65c4c7e866_15894289 (Smarty_Internal_Template $_smarty_tpl)
 
         <canvas  id="0backCanvas" width="282" height="400" class="canvas"></canvas>
         <canvas  id="1backCanvas" width="282" height="400" class="canvas"></canvas>
-        <canvas  id="2backCanvas" width="282" height="400" class="canvas"></canvas>
+        <canvas id="2backCanvas" width="282" height="400" class="canvas"></canvas>
 
         <canvas  id="backJudgeCanvas" width="282" height="400" class="canvas"></canvas>
     </div>
+
+    
+    </div>
+    
 
     <?php echo '<script'; ?>
  src="../Script/StartPage.js"><?php echo '</script'; ?>
